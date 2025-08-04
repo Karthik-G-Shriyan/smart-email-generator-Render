7efa2028c5fc14bbe42f57a1f65c6c09ef936b25
karthik <karthikgsringeri@gmail.com>
2025-08-04 23:11:25 +0530

    git status sdfs
    git status
    sdfs
    
    git status

---
+++ src/main/java/com/unicksbyte/smart_email_generator/SmartEmailGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.unicksbyte.smart_email_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+SmartEmailGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmailGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicksbyte/smart_email_generator/SmartEmailGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicksbyte.smart_email_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
